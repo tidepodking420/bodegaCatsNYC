a119c5796ed0e4f3c43f2477178db2ccf98071fb
MichaelFortanely <michaelj.fortanely@gmail.com>
2025-01-19 14:57:31 -0500

    step 1

--- bodega-cats-front-end/src/App.jsx
+++ bodega-cats-front-end/src/App.jsx
@@ -1,23 +1,13 @@
-import { useState } from 'react'
+import './App.css';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+    This is my map App
+    <Navbar/>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ bodega-cats-front-end/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import './Navbar.css';
+
+export default function Navbar(){
+  return (
+    <div className="heading">
+    <h1>This is my map App</h1>
+    </div>
+  );
+}
