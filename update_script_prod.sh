d57f30eb9ee0ffa9b6d2b5ccff4eb06cd29a109c
MichaelFortanely <michaelj.fortanely@gmail.com>
2025-02-05 17:41:37 -0500

    prod updates

---
+++ update_script_prod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull
+cp /home/ec2-userlocal_env/python.env /home/ec2-userbodegaCatsNYC/flask_app/.env
+cp /home/ec2-userlocal_env/vite.env /home/ec2-userbodegaCatsNYC/front-end/.env
