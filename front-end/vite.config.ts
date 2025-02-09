745252e820f1c2dd1d79e37ea3527e7b8d00840f
MichaelFortanely <michaelj.fortanely@gmail.com>
2025-02-09 13:18:44 -0500

    allow load balancer to work

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'cat-app-load-balancer-254988594.us-east-2.elb.amazonaws.com', // Allow the ALB hostname
+      'example.com', // Add your custom domain if you have one
+    ],
+  },
 })
