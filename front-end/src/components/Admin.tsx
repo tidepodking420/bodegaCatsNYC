22fcf754953313d96753e2efeb912a22d3bc9739
MichaelFortanely <michaelj.fortanely@gmail.com>
2025-02-10 20:29:21 -0500

    admin screen

--- front-end/src/components/Admin.tsx
+++ front-end/src/components/Admin.tsx
@@ -1,16 +1,55 @@
 const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL;
 const QUEUE_URL = VITE_SERVER_URL + "/queue"
+import {useState, useEffect} from 'react'
+import { AdminItem } from './AdminItem';
+
+export type QueueItem = {
+    id: number,
+    lat: string,
+    lng: string,
+    created_at: string,
+    catName: string,
+    catDesc: string,
+    username: string,
+    awsuuid: string
+};
+
 
 export function Admin(){
 
-    fetch(QUEUE_URL, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(res => res.json()).then(data => console.log(data))
+    function confirmOrReject(queue_id: number, selection: boolean){
+        fetch(QUEUE_URL + `?queue_id=${queue_id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json()).then(data => {
+            console.log(data)
+            // TODO confirm success from server
+            const newQueue = queue.filter(item => item.id === queue_id);
+            console.log('newQueue', selection)
+            console.log(newQueue)
+        })
+    }
+
+
+
+    const [queue, setQueue] = useState<Array<QueueItem>>([]);
+    useEffect(() => {
+        fetch(QUEUE_URL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json()).then(data => setQueue(data.queue))
+    }, [queue])
 
     return (
-        <h1>Super Secret Admin screen</h1>
+        <div>
+            <center><h1>Super Secret Admin screen</h1></center>
+            <div className='queue-grid'>
+                {queue.map(queueItem => <AdminItem key={`adminItem-${queueItem.id}`} queueItem={queueItem} confirmOrReject={confirmOrReject}/>)}
+            </div>
+        </div>
     );
 }
---
+++ front-end/src/components/AdminItem.tsx
@@ -0,0 +1,57 @@
+const S3_BUCKET = import.meta.env.VITE_S3_BUCKET;
+const REGION = import.meta.env.VITE_REGION;
+import type {QueueItem} from './Admin'
+import {useState} from 'react'
+
+const GET_PHOTO_URL = (key: number | string) => `https://${S3_BUCKET}.s3.${REGION}.amazonaws.com/${key}`
+
+
+export function AdminItem({queueItem, confirmOrReject} : {queueItem: QueueItem, confirmOrReject: any}){
+
+    const [selectedOption, setSelectedOption] = useState('');
+    const {id, lat, lng, created_at, catName, catDesc, username, awsuuid} = queueItem
+    return (
+         <div className='queue-item' key={id}>
+            <center>
+                <p style={{display: 'inline-block'}}>id: {id}</p>
+                <div style={{display: 'inline-block'}}>
+                    <label>
+                    <input 
+                        type="radio" 
+                        value="accept" 
+                        checked={selectedOption === 'accept'} 
+                        onChange={(e) => setSelectedOption(e.target.value)} 
+                    />
+                    Accept
+                    </label>
+                    <label>
+                        <input 
+                            type="radio" 
+                            value="reject" 
+                            checked={selectedOption === 'reject'} 
+                            onChange={(e) => setSelectedOption(e.target.value)} 
+                        />
+                        Reject
+                    </label>
+                </div>
+                <button onClick={() => {
+                    if(selectedOption.length === 0){
+                        alert('Choose an option')
+                        return;
+                    }
+                    confirmOrReject(id, selectedOption)
+                    }} >Confirm selection: {selectedOption}</button>
+                <div>
+                    <a href={`https://www.google.com/maps?q=${lat},${lng}`} target="_blank" rel="noopener noreferrer">
+                        lat/lng:{lat} {lng}
+                    </a>
+                </div>
+                <p>created_at: {created_at}</p>
+                <p>cat name: {catName}</p>
+                <p>cat description: {catDesc}</p>
+                <p>username: {username}</p>
+                <img src={GET_PHOTO_URL(awsuuid)} alt={`${awsuuid}`} width={'75%%'}/>
+             </center>
+         </div>
+    );
+}
